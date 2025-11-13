57f07288dd7db6927930a0341a164d633c8c3ed8
Mehdy <mehdibentaleb548@gmail.com>
2025-11-13 12:00:47 +0100

    Add Formulaires, CSRF, sessions

---
+++ blog-form/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
