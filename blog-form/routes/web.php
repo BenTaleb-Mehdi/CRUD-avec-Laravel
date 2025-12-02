3b309d93e9927f9ea6509efbd0a28ca16157f068
Mehdy <mehdibentaleb548@gmail.com>
2025-12-02 12:27:38 +0100

    Protéger l’espace d’administration avec le middleware auth

--- blog-form/routes/web.php
+++ blog-form/routes/web.php
@@ -3,13 +3,15 @@
 use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => redirect()->route('admin.dashboard'));
 
+route::middleware(['auth'])->group(function (){
+    Route::resource('articles', ArticleController::class)->except(['show']);
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
 
-Route::get('/', fn() => redirect()->route('articles.index'));
-Route::resource('articles', ArticleController::class)->except(['show']);
-
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 
 Auth::routes();
 
