cfcfb32a04f6999bd939627226235abfcadda152
Mehdy <mehdibentaleb548@gmail.com>
2025-12-03 12:13:09 +0100

    Créer des Gates

--- blog-form/app/Providers/AuthServiceProvider.php
+++ blog-form/app/Providers/AuthServiceProvider.php
@@ -6,10 +6,10 @@ use App\Models\Article;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      */
     public function register(): void
     {
@@ -17,12 +17,10 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      */
-    public function boot(): void
-    {
+    public function boot(): void {
         
-        
-        
+      
     }
 }
